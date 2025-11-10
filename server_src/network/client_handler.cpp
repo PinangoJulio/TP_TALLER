79592e90711a58ddd147ec3a93e8717fb2005980
Lourdes <146141270+loudemeglio@users.noreply.github.com>
2025-11-09 23:35:58 -0300

    Feature: implementado thread ClientHandler que controla la conexion de un cliente particular

--- server_src/network/client_handler.cpp
+++ server_src/network/client_handler.cpp
@@ -1,29 +1,30 @@
 #include "client_handler.h"
 #include <utility>
 
-ClientHandler::ClientHandler(Socket&& skt, const int id, Queue<struct Command>& queue):
-        skt(std::move(skt)),
-        client_id(id),
-        messages_queue(),
-        receiver(this->skt, this->client_id, queue),
-        sender(this->skt, messages_queue),
-        game_queue(queue) {}
+ClientHandler::ClientHandler(Socket skt, int id, MatchesMonitor& monitor, LobbyManager& lobby_manager):
+skt(std::move(skt)),
+client_id(id),
+protocol(this->skt, lobby_manager),
+monitor(monitor),
+is_alive(true),
+messages_queue(),
+lobby_manager(lobby_manager),
+receiver(protocol, this->client_id, messages_queue, is_alive, monitor, lobby_manager) {}
 
 void ClientHandler::run_threads() {
     receiver.start();
-    sender.start();
 }
 
-void ClientHandler::stop_threads() {
+void ClientHandler::stop_connection() {
+    is_alive = false;
+}
+
+bool ClientHandler::is_running() {
+    return is_alive;
+}
+
+ClientHandler::~ClientHandler() {
+    stop_connection();
     receiver.kill();
-    sender.kill();
-}
-
-void ClientHandler::join_threads() {
     receiver.join();
-    sender.join();
-}
-
-bool ClientHandler::is_alive() { 
-    return receiver.status() || sender.status(); 
 }
