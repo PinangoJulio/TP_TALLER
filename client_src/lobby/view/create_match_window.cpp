483e454d4b9f96a5474b95719703068057bb3b5c
fabivrp <fromerop@fi.uba.ar>
2025-11-25 17:39:58 -0300

    vista del lobby ajustada

--- client_src/lobby/view/create_match_window.cpp
+++ client_src/lobby/view/create_match_window.cpp
@@ -46,9 +46,9 @@ void CreateMatchWindow::loadCities()
     liberty.imagePath = "assets/img/lobby/cities/liberty-city.png";
     liberty.trackNames = {"Circuito Centro", "Ruta Costera", "Autopista Norte"};
     liberty.trackImagePaths = {
-        "assets/img/tracks/liberty_track1.png",
-        "assets/img/tracks/liberty_track2.png",
-        "assets/img/tracks/liberty_track3.png"};
+        "assets/img/map/cities/caminos/liberty-city/ruta-1/debug_resultado_v5.jpg",
+        "assets/img/map/cities/caminos/liberty-city/ruta-2/debug_resultado_v5.png",
+        "assets/img/map/cities/caminos/liberty-city/ruta-3/debug_resultado_v5.png"};
     cities.push_back(liberty);
 
     CityInfo sanAndreas;
@@ -56,19 +56,19 @@ void CreateMatchWindow::loadCities()
     sanAndreas.imagePath = "assets/img/lobby/cities/san-andreas.png";
     sanAndreas.trackNames = {"Desierto", "Ciudad", "Montaña"};
     sanAndreas.trackImagePaths = {
-        "assets/img/tracks/san_andreas_track1.png",
-        "assets/img/tracks/san_andreas_track2.png",
-        "assets/img/tracks/san_andreas_track3.png"};
+        "assets/img/map/cities/caminos/san-andreas/ruta-1/debug_resultado_v5.png",
+        "assets/img/map/cities/caminos/san-andreas/ruta-2/debug_resultado_v5.png",
+        "assets/img/map/cities/caminos/san-andreas/ruta-3/debug_resultado_v5.jpg"};
     cities.push_back(sanAndreas);
 
     CityInfo viceCity;
     viceCity.name = "Vice City";
     viceCity.imagePath = "assets/img/lobby/cities/vice-city.png";
-    viceCity.trackNames = {"Playa", "Centro", "Puentes"};
+    viceCity.trackNames = {"Centro", "Puente", "PLaya"};
     viceCity.trackImagePaths = {
-        "assets/img/tracks/vice_city_track1.png",
-        "assets/img/tracks/vice_city_track2.png",
-        "assets/img/tracks/vice_city_track3.png"};
+        "assets/img/map/cities/caminos/vice-city/ruta-1/debug_resultado_v5.jpg",
+        "assets/img/map/cities/caminos/vice-city/ruta-2/debug_resultado_v5.jpg",
+        "assets/img/map/cities/caminos/vice-city/ruta-3/debug_resultado_v5.jpg"};
     cities.push_back(viceCity);
 }
 
